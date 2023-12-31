540f4c54d1b433928bd8272d7a32edf2be28e1b4
Lukas Armstrong <Lucas.J.Armstrong@gmail.com>
2023-12-31 03:22:48 -0500

    only show plex login on local endpoint

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -66,71 +66,175 @@ const Login = () => {
     refreshWhenHidden: false,
     revalidateOnFocus: false,
   });
-
-  return (
-    <div className="relative flex min-h-screen flex-col bg-gray-900 py-14">
-      <PageTitle title={intl.formatMessage(messages.signin)} />
-      <ImageFader
-        backgroundImages={
-          backdrops?.map(
-            (backdrop) => `https://image.tmdb.org/t/p/original${backdrop}`
-          ) ?? []
-        }
-      />
-      <div className="absolute top-4 right-4 z-50">
-        <LanguagePicker />
-      </div>
-      <div className="relative z-40 mt-10 flex flex-col items-center px-4 sm:mx-auto sm:w-full sm:max-w-md">
-        <img src="/logo_stacked.svg" className="mb-10 max-w-full" alt="Logo" />
-        <h2 className="mt-2 text-center text-3xl font-extrabold leading-9 text-gray-100">
-          {intl.formatMessage(messages.signinheader)}
-        </h2>
-      </div>
-      <div className="relative z-50 mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div
-          className="bg-gray-800 bg-opacity-50 shadow sm:rounded-lg"
-          style={{ backdropFilter: 'blur(5px)' }}
-        >
-          <>
-            <Transition
-              as="div"
-              show={!!error}
-              enter="transition-opacity duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="mb-4 rounded-md bg-red-600 p-4">
-                <div className="flex">
-                  <div className="flex-shrink-0">
-                    <XCircleIcon className="h-5 w-5 text-red-300" />
-                  </div>
-                  <div className="ml-3">
-                    <h3 className="text-sm font-medium text-red-300">
-                      {error}
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </Transition>
-            <div>
-              <>
-                <div>
-                  <div>
-                    <div className="px-10 py-8">
-                      <LocalLogin revalidate={revalidate} />
+  if (location.hostname !== "192.168.3.63"){
+    return (
+      <div className="relative flex min-h-screen flex-col bg-gray-900 py-14">
+        <PageTitle title={intl.formatMessage(messages.signin)} />
+        <ImageFader
+          backgroundImages={
+            backdrops?.map(
+              (backdrop) => `https://image.tmdb.org/t/p/original${backdrop}`
+            ) ?? []
+          }
+        />
+        <div className="absolute top-4 right-4 z-50">
+          <LanguagePicker />
+        </div>
+        <div className="relative z-40 mt-10 flex flex-col items-center px-4 sm:mx-auto sm:w-full sm:max-w-md">
+          <img src="/logo_stacked.svg" className="mb-10 max-w-full" alt="Logo" />
+          <h2 className="mt-2 text-center text-3xl font-extrabold leading-9 text-gray-100">
+            {intl.formatMessage(messages.signinheader)}
+          </h2>
+        </div>
+        <div className="relative z-50 mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div
+            className="bg-gray-800 bg-opacity-50 shadow sm:rounded-lg"
+            style={{ backdropFilter: 'blur(5px)' }}
+          >
+            <>
+              <Transition
+                as="div"
+                show={!!error}
+                enter="transition-opacity duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="mb-4 rounded-md bg-red-600 p-4">
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <XCircleIcon className="h-5 w-5 text-red-300" />
+                    </div>
+                    <div className="ml-3">
+                      <h3 className="text-sm font-medium text-red-300">
+                        {error}
+                      </h3>
                     </div>
                   </div>
                 </div>
-              </>
-            </div>
-          </>
+              </Transition>
+              <div>
+                <>
+                  <div>
+                    <div>
+                      <div className="px-10 py-8">
+                        <LocalLogin revalidate={revalidate} />
+                      </div>
+                    </div>
+                  </div>
+                </>
+              </div>
+            </>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  else{
+    return (
+      <div className="relative flex min-h-screen flex-col bg-gray-900 py-14">
+        <PageTitle title={intl.formatMessage(messages.signin)} />
+        <ImageFader
+          backgroundImages={
+            backdrops?.map(
+              (backdrop) => `https://image.tmdb.org/t/p/original${backdrop}`
+            ) ?? []
+          }
+        />
+        <div className="absolute top-4 right-4 z-50">
+          <LanguagePicker />
+        </div>
+        <div className="relative z-40 mt-10 flex flex-col items-center px-4 sm:mx-auto sm:w-full sm:max-w-md">
+          <img src="/logo_stacked.svg" className="mb-10 max-w-full" alt="Logo" />
+          <h2 className="mt-2 text-center text-3xl font-extrabold leading-9 text-gray-100">
+            {intl.formatMessage(messages.signinheader)}
+          </h2>
+        </div>
+        <div className="relative z-50 mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div
+            className="bg-gray-800 bg-opacity-50 shadow sm:rounded-lg"
+            style={{ backdropFilter: 'blur(5px)' }}
+          >
+            <>
+              <Transition
+                as="div"
+                show={!!error}
+                enter="transition-opacity duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="mb-4 rounded-md bg-red-600 p-4">
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <XCircleIcon className="h-5 w-5 text-red-300" />
+                    </div>
+                    <div className="ml-3">
+                      <h3 className="text-sm font-medium text-red-300">
+                        {error}
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              </Transition>
+              <Accordion single atLeastOne>
+                {({ openIndexes, handleClick, AccordionContent }) => (
+                  <>
+                    <button
+                      className={`w-full cursor-default bg-gray-800 bg-opacity-70 py-2 text-center text-sm font-bold text-gray-400 transition-colors duration-200 focus:outline-none sm:rounded-t-lg ${
+                        openIndexes.includes(0) && 'text-indigo-500'
+                      } ${
+                        settings.currentSettings.localLogin &&
+                        'hover:cursor-pointer hover:bg-gray-700'
+                      }`}
+                      onClick={() => handleClick(0)}
+                      disabled={!settings.currentSettings.localLogin}
+                    >
+                      {intl.formatMessage(messages.signinwithplex)}
+                    </button>
+                    <AccordionContent isOpen={openIndexes.includes(0)}>
+                      <div className="px-10 py-8">
+                        <PlexLoginButton
+                          isProcessing={isProcessing}
+                          onAuthToken={(authToken) => setAuthToken(authToken)}
+                        />
+                      </div>
+                    </AccordionContent>
+                    {settings.currentSettings.localLogin && (
+                      <div>
+                        <button
+                          className={`w-full cursor-default bg-gray-800 bg-opacity-70 py-2 text-center text-sm font-bold text-gray-400 transition-colors duration-200 hover:cursor-pointer hover:bg-gray-700 focus:outline-none ${
+                            openIndexes.includes(1)
+                              ? 'text-indigo-500'
+                              : 'sm:rounded-b-lg'
+                          }`}
+                          onClick={() => handleClick(1)}
+                        >
+                          {intl.formatMessage(messages.signinwithoverseerr, {
+                            applicationTitle:
+                              settings.currentSettings.applicationTitle,
+                          })}
+                        </button>
+                        <AccordionContent isOpen={openIndexes.includes(1)}>
+                          <div className="px-10 py-8">
+                            <LocalLogin revalidate={revalidate} />
+                          </div>
+                        </AccordionContent>
+                      </div>
+                    )}
+                  </>
+                )}
+              </Accordion>
+            </>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Login;
